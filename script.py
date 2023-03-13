959960b79983afe4b3088ab50ad951c717bb1174
ElVejigante <ernesto.cancel@gmail.com>
2023-03-13 19:44:04 -0400

    get and print files list

--- script.py
+++ script.py
@@ -17,3 +17,8 @@ with open(local_path, "wb") as f:
 
 # Import the required method
 from zipfile import ZipFile
+
+with ZipFile(path, mode="r") as f:
+    #Get the list of files and print it
+    file_names = f.namelist()
+    print(file_names)
