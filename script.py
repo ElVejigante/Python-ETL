606ac5c0e7cc74932865115dd596e122148ef1e1
ElVejigante <ernesto.cancel@gmail.com>
2023-03-13 19:47:14 -0400

    extract csv file

--- script.py
+++ script.py
@@ -19,6 +19,9 @@ with open(local_path, "wb") as f:
 from zipfile import ZipFile
 
 with ZipFile(path, mode="r") as f:
-    #Get the list of files and print it
+    # Get the list of files and print it
     file_names = f.namelist()
     print(file_names)
+    # Extract the CSV file
+    csv_file_path = f.extract(file_names[0])
+    print(csv_file_path)
