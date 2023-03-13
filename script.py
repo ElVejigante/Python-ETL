7e4ec4169db15c1c668b624e95c47c3beeb7aba9
ElVejigante <ernesto.cancel@gmail.com>
2023-03-13 13:52:19 -0400

    print status code

--- script.py
+++ script.py
@@ -5,3 +5,5 @@ import requests
 path = "https://assets.datacamp.com/production/repositories/5899/datasets/19d6cf619d6a771314f0eb489262a31f89c424c2/ppr-all.zip"
 response = requests.get(path)
 
+# Print the status code
+print(response.status_code)
