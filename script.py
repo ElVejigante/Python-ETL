c4402f91fcdb8bb746e3dfe7566843c3e20a9dac
ElVejigante <ernesto.cancel@gmail.com>
2023-03-13 13:49:15 -0400

    get zip file

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
 # Import the required library
 import requests
+
+# Get the zip file (create path)
+path = "https://assets.datacamp.com/production/repositories/5899/datasets/19d6cf619d6a771314f0eb489262a31f89c424c2/ppr-all.zip"
+response = requests.get(path)
+
