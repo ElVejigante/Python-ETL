efae014a53304b5ab787563ecd2b52b1dd27cc20
ElVejigante <ernesto.cancel@gmail.com>
2023-03-14 14:05:49 -0400

    paths defined

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,3 +1,9 @@
 import os
 import requests
 
+# Paths
+
+base_path = "/home/repl/workspace"
+source_url = "https://assets.datacamp.com/production/repositories/5899/datasets/66691278303f789ca4acd3c6406baa5fc6adaf28/PPR-ALL.zip"
+source_path = f"{base_path}/data/source/downloaded_at=2021-01-01/ppr-all.zip"
+
