80279b0dbb023721d303cb2ff0afd4daf9b659c3
ElVejigante <ernesto.cancel@gmail.com>
2023-03-20 09:54:08 -0400

    date format updates

--- workspace/scripts/transform.py
+++ workspace/scripts/transform.py
@@ -1,4 +1,15 @@
 from datetime import datetime
 
 def transform_case(input_string):
-    
+    """
+    Lowercase string fields
+    """
+    return input_string.lower()
+
+def update_date_of_sale(date_input):
+    """
+    Updates date format from DD/MM/yyyy to YYYY-MM-DD
+    """
+    current_format = datetime.strptime(date_input, "%d/%m/%Y")
+    new_format = current_format.strftime("%Y-%m-%d")
+    return new_format
